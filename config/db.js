a596fc6942eb31a86869afcf3eac750057665b8e
Sarthak <sarthaknagare96@gmail.com>
2026-08-01 23:41:36 +0530

    small change

--- config/db.js
+++ config/db.js
@@ -3,9 +3,9 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI);
-    console.log("✅ MongoDB Connected");
+    console.log("MongoDB Connected");
   } catch (error) {
-    console.error("❌ MongoDB Connection Failed");
+    console.error("MongoDB Connection Failed");
     console.error(error.message);
     process.exit(1);
   }

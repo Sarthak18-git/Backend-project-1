e50c86c4ff10c3d577bc0cb18fd604dfd398060c
Sarthak <sarthaknagare96@gmail.com>
2026-08-02 16:10:40 +0530

    All main apis created

--- controllers/note.controller.js
+++ controllers/note.controller.js
@@ -16,4 +16,71 @@ const createNote=async (req,res)=>{
        });
     }
 };
-module.exports= {createNote,};
+const getAllNotes=async (req,res)=>{
+    try{
+        const notes=await Note.find();
+        res.status(201).json({
+            success:true,
+            message:"all notes fetched",
+            notes
+        }); 
+    }
+    catch(err){
+        res.status(500).json({
+            success:false,
+            message:err.message,
+        });
+    }
+}
+const deleteNote=async(req,res)=>{
+    try{
+        const {id}=req.params;
+        const note=await Note.deleteOne({ _id: id });
+        res.status(200).json({
+            success:true,
+            message:"Note deleted successfully",
+            note
+        });
+    }
+    catch(err){
+        res.status(500).json({
+            success:false,
+            message:err.message,
+        });
+    }
+  }
+  const updateNote=async(req,res)=>{
+      try{
+         const {id}=req.params;
+         const {title,description}=req.body;
+         const result=await Note.updateOne({_id:id},
+            {
+                title,
+                description,
+            },
+            {
+                runValidators:true,
+            }
+         );
+         if (result.matchedCount === 0) {
+            return res.status(404).json({
+                success: false,
+                message: "Note not found",
+            });
+        }
+
+         res.status(200).json({
+            success:true,
+            message:"Note updated successfully",
+            result
+         });
+      }
+      catch(err){
+        res.status(500).json({
+            success:false,
+            message:err.message,
+        });
+      }
+  }
+
+module.exports= {createNote, getAllNotes,deleteNote,updateNote};
--- routes/note.routes.js
+++ routes/note.routes.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const{createNote}=require("../controllers/note.controller");
+const{createNote,getAllNotes,deleteNote,updateNote}=require("../controllers/note.controller");
 router.post("/notes",createNote);
+router.get("/notes",getAllNotes);
+router.delete("/notes/:id",deleteNote);
+router.put("/notes/:id",updateNote);
 module.exports=router;

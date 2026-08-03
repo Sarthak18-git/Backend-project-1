6d49b4edd9bf167233cfd013f73705adfeff7679
Sarthak <sarthaknagare96@gmail.com>
2026-08-03 16:27:11 +0530

    global error handling changes

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const app = express();
 const noteroutes = require("./routes/note.routes");
+const errorHandler =require("./middleware/error.middle");
 app.use(express.json());
 app.use("/api",noteroutes);
+app.use(errorHandler);
 module.exports = app;
